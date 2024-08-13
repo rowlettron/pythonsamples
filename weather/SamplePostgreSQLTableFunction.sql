6b7ecaf2f27c8592acf4801763f00121559758f8
rowlettron <rowlettron@yahoo.com>
2024-08-13 15:20:40 -0500

    New files

--- weather/SamplePostgreSQLTableFunction.sql
+++ weather/SamplePostgreSQLTableFunction.sql
@@ -6,30 +6,30 @@ $$
 begin
     create temporary table rentaloutput (
         title varchar(255) null,
-		totalRentals bigint null
-	);
-	
+        totalRentals bigint null
+    );
+
     if _title is null then
-	    insert into rentaloutput(title, totalRentals)
-			select f.title, count(rental_id) as totalRentals
-		    from public.film f
-		    inner join public.inventory i on f.film_id = i.film_id
-		    inner join public.rental r on i.inventory_id = r.inventory_id
-    		group by f.title;
-	else
-		insert into rentaloutput(title, totalRentals)
-			select f.title, count(rental_id) as totalRentals
-		    from public.film f
-		    inner join public.inventory i on f.film_id = i.film_id
-		    inner join public.rental r on i.inventory_id = r.inventory_id
-		    where f.title = _title
-		    group by f.title;
-	end if;
-	
-    return query 
-	select *
-	from rentaloutput;
-	
+        insert into rentaloutput(title, totalRentals)
+            select f.title, count(rental_id) as totalRentals
+            from public.film f
+            inner join public.inventory i on f.film_id = i.film_id
+            inner join public.rental r on i.inventory_id = r.inventory_id
+            group by f.title;
+    else
+        insert into rentaloutput(title, totalRentals)
+            select f.title, count(rental_id) as totalRentals
+            from public.film f
+            inner join public.inventory i on f.film_id = i.film_id
+            inner join public.rental r on i.inventory_id = r.inventory_id
+            where f.title = _title
+            group by f.title;
+    end if;
+
+    return query
+    select *
+    from rentaloutput;
+
 end;
 $$
 language plpgsql;
