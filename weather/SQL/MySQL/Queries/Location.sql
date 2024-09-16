c57d838dff3e3f343f44e508c26f33f4d30965a5
rowlettron <rowlettron@yahoo.com>
2024-09-16 10:54:20 -0500

    Added Location.sql

---
+++ weather/SQL/MySQL/Queries/Location.sql
@@ -0,0 +1,13 @@
+use Weather_v2;
+
+select jsondata ->> '$.location.name' as locationName,
+       jsondata ->> '$.location.region' as region,
+       jsondata ->> '$.location.country' as country,
+       jsondata -> '$.location.lat' as latitude,
+       jsondata -> '$.location.lon' as longitude,
+       jsondata ->> '$.location.tz_id' as timeZone,
+       jsondata -> '$.location.localtime_epoch' as localTimeEpoch,
+       jsondata ->> '$.location.localtime' as local_Time
+
+from Weather_v2.weatherjsonload;
+
