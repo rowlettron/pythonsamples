2dca0537141aecbe905346b19e2c1a77c565afc2
rowlettron <rowlettron@yahoo.com>
2024-09-23 16:31:39 -0500

    updated files

--- weather/SQL/MySQL/Tables/HourlyForecast.sql
+++ weather/SQL/MySQL/Tables/HourlyForecast.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS HourlyForecast;
 CREATE TABLE HourlyForecast (
     HourlyForecastID INT AUTO_INCREMENT,
     LocationID INT NULL,
-    forecast_date DATETIME NULL,
     forecast_hour DATETIME NULL,
     forecast_time_epoch INT NULL,
     temp_c NUMERIC(6, 2) NULL,
@@ -20,6 +19,7 @@ CREATE TABLE HourlyForecast (
     pressure_in NUMERIC(6, 2) NULL,
     precip_mm NUMERIC(6, 2) NULL,
     precip_in NUMERIC(6, 2) NULL,
+    snow_cm NUMERIC(6,2) NULL, 
     humidity NUMERIC(6, 2) NULL,
     cloud NUMERIC(6, 2) NULL,
     feelslike_c NUMERIC(6, 2) NULL,
@@ -39,7 +39,8 @@ CREATE TABLE HourlyForecast (
     gust_mph NUMERIC(6, 2) NULL,
     gust_kph NUMERIC(6, 2) NULL,
     uv NUMERIC(6, 2) NULL,
-    CONSTRAINT PK_HourlyForecast PRIMARY KEY (HourlyForecastID ASC)
+    CONSTRAINT PK_HourlyForecast PRIMARY KEY (HourlyForecastID ASC), 
+    UNIQUE KEY (LocationID, forecast_time_epoch)
     );
 
 
